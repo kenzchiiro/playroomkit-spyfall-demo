b6eb201243ef551ae89119060e2ddcd7bbe4a6d5
kench1r0 <nattawut.eleven@gmail.com>
2024-07-28 19:20:47 +0700

    feat: update card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Card from './components/Card';
 
 function App() {
-  const players = [
-    { isMyCard:true , name: "player 1", "place": "โรงเรียน", "role": "ผอ.", "isHost": true },
-    { isMyCard:true , name: "player 2", "place": "โรงเรียน", "role": "นักเรียน", "isHost": false },
-    { isMyCard:true , name: "player 3", "place": "", "role": "", "isHost": false },
-  ]
+  const dataState = {
+    player1 : { place: "โรงเรียน", role: "ผอ." },
+    player2 : { place: "โรงเรียน", role: "นักเรียน" },
+    player3 : { place: "", role: "" }
+  }
 
   return (
     <div>
@@ -19,8 +19,8 @@ function App() {
       <div className="flex flex-col pattern-paper pattern-gray-100 pattern-bg-white pattern-size-20 pattern-opacity-100 h-svh">
         <div className="w-screen justify-center p-2 mt-16">
           <div className="w-full carousel carousel-center space-x-2 justify-center">
-            {players.map(player => {
-                return <Card data={player} />
+          {Object.keys(dataState).map(playerName => {
+                return <Card owner={playerName} data={dataState[playerName]} />
             })}
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,19 @@
-function Card({ data }) {
-  return (
-    <div className="flex flex-col carousel-item border-4 rounded-xl border-slate-600">
-      <div className="flex w-64 justify-between ml-1 items-center font-extrabold text-lg md:text-1xl text-neutral text-left z-40">
-        <div className="flex space-x-2 m-2">{data.isHost? data.name+"(Host)":data.name}</div>
-      </div>
-      <div className="h-80 content-center">
-        <div className="h-full content-center">
-          <div className="flex justify-center text-2xl">{data.place}</div>
-          <br/>
-          <div className="flex justify-center text-2xl">{data.role}</div>
+function Card({ owner, data }) {
+    return (
+      <div className="flex flex-col carousel-item border-4 rounded-xl border-slate-600">
+        <div className="flex w-64 justify-between ml-1 items-center font-extrabold text-lg md:text-1xl text-neutral text-left z-40">
+          <div className="flex space-x-2 m-2">{!data.isHost? owner+"(Host)":owner}</div>
+        </div>
+        <div className="h-80 content-center">
+          <div className="h-full content-center">
+            <div className="flex justify-center text-2xl">{data.place}</div>
+            <br/>
+            <div className="flex justify-center text-2xl">{data.role}</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-export default Card;
+    );
+  }
+  
+  export default Card;
+  
