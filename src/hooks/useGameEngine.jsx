38430be91c9d54e379e1a8092ee034d6b60aa1b3
kench1r0 <nattawut.eleven@gmail.com>
2024-07-28 18:23:35 +0700

    feat: add func random player

--- src/hooks/useGameEngine.jsx
+++ src/hooks/useGameEngine.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { randInt } from 'three/src/math/MathUtils.js';
+import data from '../data.json';
+import {
+    isHost,
+    useMultiplayerState,
+    usePlayersList,
+} from 'playroomkit';
 
 const GameEngineContext = React.createContext();
 
 export const GameEngineProvider = ({ children }) => {    
-    const gameState = {};
+    // set state in game
+    const [cards, setCards] = useMultiplayerState("cards", data.cards);
+    const [cardsOwner, setCardsOwner] = useMultiplayerState("cardsOwner", []);
+    const [state, setState] = useMultiplayerState("state", "");
+
+    // collect players data from playroomkit
+    const players = usePlayersList(true)
+    players.sort((a, b) => a.id.localeCompare(b.id));
+
+    // set state for export to others
+    const gameState = {
+        players,
+        cards,
+        cardsOwner,
+    };
+
+
+    const randomeCards = (players) => {
+        // random spy by player index
+        const randomSpyIndex = randInt(0, players.length - 1);
+
+        // init empty card
+        var card = {};
+        // random place
+        const randomPlaceIndex = randInt(0, cards.length - 1);
+        const place = data[randomPlaceIndex].place
+
+        // set card each player
+        players.forEach((index, player) => {
+          if (index===randomSpyIndex){
+            card["palce"] = ""
+            card[player.getProfile().name] = ""
+          }else{
+            card["palce"] = place
+            const randomRoleIndex = randInt(0, place.roles.length - 1);
+            card[player.getProfile().name] = place.role[randomRoleIndex]
+          }
+          setCardsOwner(card,true)
+          player.setState("status","")
+        });
+        return
+    }
 
 
     return (
