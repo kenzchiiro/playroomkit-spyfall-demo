234423de7eb80470d2efaf0b91e0c992526454b5
kench1r0 <nattawut.eleven@gmail.com>
2024-07-28 16:29:27 +0700

    feat: set template

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import Card from './components/Card';
 
 function App() {
-  const players = [{name:"player 1","word":"word 1"},{name:"player 2","word":"word 2"}]
+  const players = [
+    { isMyCard:true , name: "player 1", "place": "โรงเรียน", "role": "ผอ.", "isHost": true },
+    { isMyCard:true , name: "player 2", "place": "โรงเรียน", "role": "นักเรียน", "isHost": false },
+    { isMyCard:true , name: "player 3", "place": "", "role": "", "isHost": false },
+  ]
+
   return (
-    <div style={{ backgroundColor: "#FFFFFF"}}>
+    <div>
+      <div className="h-fit w-full grid justify-items-end">
+        <button
+          className="btn btn-outline btn-green-100 btn-md m-4 border-4"
+        >
+          RESTART
+        </button>
+      </div>
       <div className="flex flex-col pattern-paper pattern-gray-100 pattern-bg-white pattern-size-20 pattern-opacity-100 h-svh">
-        <div className="w-screen justify-center p-2">
-          <div className="h-fit mb-4 w-full grid justify-items-end">
-            <button
-              className="btn btn-outline btn-neutral btn-md m-4"
-              >
-              RESTART
-            </button>
-          </div>
+        <div className="w-screen justify-center p-2 mt-16">
           <div className="w-full carousel carousel-center space-x-2 justify-center">
             {players.map(player => {
-                return <Card data={player}/>
+                return <Card data={player} />
             })}
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 function Card({ data }) {
   return (
-    <div
-      className="w-fit flex flex-col carousel-item border-4 rounded-xl border-slate-600"
-    >
+    <div className="flex flex-col carousel-item border-4 rounded-xl border-slate-600">
       <div className="flex w-64 justify-between ml-1 items-center font-extrabold text-lg md:text-1xl text-neutral text-left z-40">
-        <div className="flex space-x-2 m-2">{data.name}</div>
+        <div className="flex space-x-2 m-2">{data.isHost? data.name+"(Host)":data.name}</div>
       </div>
       <div className="h-80 content-center">
-        <div className="mt-2 h-full content-center">
-          <div class="flex justify-center">{data.word}</div>
+        <div className="h-full content-center">
+          <div className="flex justify-center text-2xl">{data.place}</div>
+          <br/>
+          <div className="flex justify-center text-2xl">{data.role}</div>
         </div>
       </div>
     </div>
