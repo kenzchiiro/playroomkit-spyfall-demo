2e90e82e75c4c4bdc770494992abd439340276d2
kench1r0 <nattawut.eleven@gmail.com>
2024-07-29 20:04:00 +0700

    feat: update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ function Card({ owner, data, isHost }) {
   return (
     <div className="flex flex-col carousel-item border-4 rounded-xl border-slate-600">
       <div className="flex w-64 justify-between ml-1 items-center font-extrabold text-lg md:text-1xl text-neutral text-left z-40">
-        <div className="flex space-x-2 m-2">{!isHost? owner+"(Host)":owner}</div>
+        <div className="flex space-x-2 m-2">{isHost? owner+"(Host)":owner}</div>
       </div>
       <div className="h-80 content-center">
         <div className="h-full content-center">
