557981b6304b2bafb74816fc62a24b3172a79eff
kench1r0 <nattawut.eleven@gmail.com>
2024-08-10 16:43:22 +0700

    feat: update path url for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/playroomkit-spyfall-demo",
   plugins: [react()],
 })
